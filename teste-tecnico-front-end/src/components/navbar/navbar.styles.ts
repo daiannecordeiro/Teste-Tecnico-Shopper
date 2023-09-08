c2a17e0bcef1886f679165e309f598104bf3e71c
Daianne Cordeiro <daianne.nc@gmail.com>
2023-09-08 04:08:33 -0300

    feat: add navbar component

---
+++ teste-tecnico-front-end/src/components/navbar/navbar.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const NavbarContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100vw;
+	padding: 1rem 3rem;
+	color: #fff;
+	h1 {
+		font-weight: 400;
+	}
+`;
---
+++ teste-tecnico-front-end/src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+import { NavbarContainer} from './navbar.styles';
+
+export const Navbar: FC = () => {
+
+	return (
+		<NavbarContainer>
+			<h1>Sistema de Gerenciamento de Produtos</h1>
+		</NavbarContainer>
+	);
+};

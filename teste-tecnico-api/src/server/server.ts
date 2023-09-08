b1c3d41eedb278448702361564099ca83d26bbdf
Daianne Cordeiro <daianne.nc@gmail.com>
2023-09-08 04:06:10 -0300

    fix: cors error

--- teste-tecnico-api/src/server/server.ts
+++ teste-tecnico-api/src/server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 
 import { router } from './routes';
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
